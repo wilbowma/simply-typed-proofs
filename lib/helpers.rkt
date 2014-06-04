788f1516028528038a1d4c13425fab5d2f1a0677
William J. Bowman <wjb@williamjbowman.com>
2014-06-04 14:44:18 +0200

    Added docs to helpers

--- lib/helpers.rkt
+++ lib/helpers.rkt
@@ -1,4 +1,5 @@
 #lang racket
+;; Random helpers taht should be included in some standard library.
 
 (require
   redex/reduction-semantics
@@ -9,7 +10,11 @@
   test-redex-equal
   test-redex-set=?)
 
+;; Testing equality of two redex terms shouldn't require writing (term )
+;; everywhere
 (define-syntax-rule (test-redex-equal e1 e2)
   (test-equal (term e1) (term e2)))
+
+;; Sometimes I have to deal with sets of things.
 (define-syntax-rule (test-redex-set=? e1 e2)
   (check set=? (list->set (term e1)) (list->set (term e2))))
